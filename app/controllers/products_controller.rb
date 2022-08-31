15139650bd4a1d9e1a6d45a3678d1890c0565ac4
Marcus Davis <marcusdavis1018@gmail.com>
2022-08-31 13:53:14 -0400

    how to search by id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,8 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
-  def first_product
-    first_product = Product.first
-    render json: first_product.as_json
-  end
-
-  def second_product
-    second_product = Product.second
-    render json: second_product.as_json
-  end
-
-  def third_product
-    third_product = Product.third
-    render json: third_product.as_json
+  def one_product
+    one_product = Product.find_by(id: params["id"])
+    render json: one_product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/all_product", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
-  get "/third_product", controller: "products", action: "third_product"
+  get "/one_product/:id" => "products#one_product"
 end

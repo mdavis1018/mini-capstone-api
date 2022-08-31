f51db6d3ef1b2b8204e9f09a9694909c5f9e2b31
Marcus Davis <marcusdavis1018@gmail.com>
2022-08-31 12:37:08 -0400

    Database products created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(
+  name: "Vans",
+  price: 110,
+  image_url: "https://www.vans.com/en-us/customs-c0004/vans-x-stranger-things-customs-poster-art-old-skool-pvn0a3vc1167?recipe=34e5499e55d5127af1004fa52cfa71a0",
+  description: "A great shoe for back to school!",
+)
+Product.create(
+  name: "Sperry",
+  price: 100,
+  image_url: "https://www.sperry.com/en/authentic-original-boat-shoe/10118M.html?dwvar_10118M_color=0197640#cgid=men-shoes-boat-shoes&start=1",
+  description: "A great shoe for style!",
+)
+Product.create(
+  name: "UGG ",
+  price: 105,
+  image_url: "https://www.ugg.com/master-sale/westsider-neumel-weather/1120902.html?dwvar_1120902_color=TSD",
+  description: "A great shoe for winter",
+)
+p "Hello"
